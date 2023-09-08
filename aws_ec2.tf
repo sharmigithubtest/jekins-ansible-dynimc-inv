fb3e8ce3fbd797345f5ba4bfee3fea6a1d6fac7f
sharmigithubtest <139699569+sharmigithubtest@users.noreply.github.com>
2023-09-08 20:27:46 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,14 +1,15 @@
-provider "aws"{
-region = "ap-south-1"
+provider"aws"{
+region= "ap-south-1"
 }
-
-
 resource "aws_instance" "AWSServer"{
 ami = "ami-02bb7d8191b50f4bb"
 instance_type = "t2.micro"
-security_groups = ["launch-wizard-2"]
-key_name = "devopsmithuntechnologies"
+security_groups= ["launch-wizard-2"]
+key_name = "devopssharmi"
 tags = {
-Name =  "tomcatservers"
+Name = "tomcatservers"
 }
 }
+ec2instancecreation.tf
+
+ 
